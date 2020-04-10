8a6a1db60edc3a5180d3db12bd48a5eca2cef719
Nepaul-roshan <56953229+Nepaul-roshan@users.noreply.github.com>
2020-04-10 14:28:46 +0200

    Add files via upload

---
+++ Emissions.java
@@ -0,0 +1,8 @@
+public class Emissions {
+	private String type;
+	
+	public Emissions(String type) {
+		this.type=type;
+	}
+}
+
---
+++ Radiations.java
@@ -0,0 +1,8 @@
+
+public class Radiations {
+	private int niveau;
+	
+	public Radiations(int niveau) {
+		this.niveau=niveau;
+	}
+}
